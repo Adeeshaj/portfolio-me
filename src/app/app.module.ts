ecc4dee34d1faba8589e5869c84965fb8799c1d4
Adeeshaj <adeesha.14@cse.mrt.ac.lk>
2019-11-07 01:14:08 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,
+  RouterModule.forRoot([
+      { path: '', component: WelcomeComponent },
+    ])
+  ],
+  declarations: [ AppComponent, HelloComponent, TopBarComponent, WelcomeComponent, AboutComponent, ProjectsComponent, ContactComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
